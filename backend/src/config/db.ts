dda2ec722183c0fdacc51ba729464f4d1c529825
Yash <137068938+YashTirpude@users.noreply.github.com>
2025-04-22 17:40:41 +0530

    deployment setup cleanup before deploying the application

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+// Load environment variables from .env file
 dotenv.config();
+
+// MongoDB connection function
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI as string, {
-      serverSelectionTimeoutMS: 5000, // Fail fast if no connection
-      maxPoolSize: 10, // Limit connections
-      socketTimeoutMS: 45000, // Close idle connections
-    });
+    // Connect to MongoDB using the Mongoose library'
+    const connect = await mongoose.connect(process.env.MONGO_URI as string);
     console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error: any) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`Error: ${error.message}`); // Log the error if connection fails
+    process.exit(1); // Exit the process if there's an error connecting to MongoDB
   }
 };
 
+// Export the MongoDB connection function
 export default connectDB;
---
+++ backend/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cors from "cors";
+import connectDB from "./config/db";
+import applicationRoutes from "./routes/applicationRoutes";
+import passport from "./config/passportConfig";
+import authRoutes from "./routes/authRoutes";
+
+dotenv.config();
+
+connectDB(); // Establish the MongoDB connection
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://job-application-tracker-orcin.vercel.app",
+    credentials: true,
+  })
+); // This allows all origins. You can configure it to be more restrictive if needed.
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+
+    cookie: { secure: false, maxAge: 15 * 24 * 60 * 60 * 1000 }, // 15 days
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session()); // Enable persistent login
+app.use("/auth", authRoutes); // Use authentication routes
+
+app.use("/applications", applicationRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
