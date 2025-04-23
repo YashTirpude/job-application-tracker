12e63eae011bf1978a043bb6fc2d398132cac7e0
Yash <137068938+YashTirpude@users.noreply.github.com>
2025-04-23 15:26:08 +0530

    revert passport config

--- backend/src/config/passportConfig.ts
+++ backend/src/config/passportConfig.ts
@@ -6,11 +6,7 @@ import cloudinary from "./cloudinaryConfig";
 
 dotenv.config();
 
-if (
-  !process.env.GOOGLE_CLIENT_ID ||
-  !process.env.GOOGLE_CLIENT_SECRET ||
-  !process.env.GOOGLE_CALLBACK_URL
-) {
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   throw new Error("Missing Google OAuth environment variables");
 }
 
@@ -19,7 +15,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL as string,
+      callbackURL:
+        "https://job-application-tracker-backend.vercel.app/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
